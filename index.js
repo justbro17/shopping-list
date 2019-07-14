3f4ce5e9d78c8378e7b13b858b35aac9a539560a
Justin Brozzetti <justbro17@gmail.com>
2019-07-15 00:03:29 +0300

    Update index.js
    add all of the JavaScript to page

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-$(function() {
+$(document).ready(function() {
+    
+    //delete item
+    $('.shopping-list').on('click', '.shopping-item-delete', function() {
+        $(this).closest('li').remove();
+    });
+
+    //strikethrough
+    $('.shopping-list').on('click', '.shopping-item-toggle', function () {
+        $(this).parent().prev().toggleClass('shopping-item__checked');
+    });
+
+    $('#js-shopping-list-form').submit(event => {
+        event.preventDefault();
+        //saves the user input to a variable 
+        let item = $('#shopping-list-entry').val();
+        //adds the user input as a li
+        $('.shopping-list').append('<li><span class="shopping-item">' + item + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+    });
 });
